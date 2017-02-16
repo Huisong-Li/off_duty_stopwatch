6e37cfdab407f9c4fa5b4a57dacb0081498fac06
lhs <lihuisong@custouch.com>
2017-02-16 15:10:05 +0800

    new file:   off_duty.py

---
+++ off_duty.py
@@ -0,0 +1,39 @@
+# coding: UTF-8
+# author: huisong.li
+# Date  : 02/10/2017
+
+from datetime import datetime
+
+def fill(number):
+    return str(number).zfill(2)
+
+def count(off_time):
+    clock = datetime.now()
+    delta = off_time - clock
+    while delta.total_seconds() > 0:
+        clock = datetime.now()
+        delta = off_time - clock
+        print (delta, end = '\r')
+
+def set_time():
+    print ('INPUT OFF TIME')
+    off_time = input().split(':')
+    length = len(off_time)
+    
+    hour   = int(off_time[0]) if off_time[0] else 20
+    minute = int(off_time[1]) if length > 1  else 0
+    second = int(off_time[2]) if length > 2  else 0
+    
+    today = datetime.now()
+    
+    return datetime(today.year, today.month, today.day,
+                    hour      , minute     , second)
+
+def main():
+    try:
+        count(set_time())
+    except KeyboardInterrupt:
+        print('\nOFF DUTY EARLY\n')
+
+if __name__ == '__main__':
+    main()
